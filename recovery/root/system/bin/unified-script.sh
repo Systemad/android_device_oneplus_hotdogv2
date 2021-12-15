489aea1f6c6b402665fe372ce7bda013be0a5589
systemad <dannesuke@gmail.com>
2021-12-15 15:02:14 +0100

    Use updated unified script by osm0sis

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-# This script is needed to automatically set device props.
+# automatically set device props for unified tree shared-hardware models/variants
 
 
 load_op7tpro()
@@ -32,17 +32,20 @@ load_op7t()
     resetprop "ro.display.series" "OnePlus 7T"
 }
 
-project=$(getprop ro.boot.project_name)
-echo $project
+project=$(getprop ro.boot.project_codename)
+echo "Running unified/variant script with $project..." >> /tmp/recovery.log
 
 case $project in
-    "19801")
-        load_op7tpro
-        ;;
-    "18865")
+    hotdogb)
         load_op7t
         ;;
-    *)
+    hotdogg)
+        load_op7tpro5g
+        ;;
+    *)  # hotdog and fallback
+        load_op7tpro
+        ;;
+
 esac
 
 exit 0
